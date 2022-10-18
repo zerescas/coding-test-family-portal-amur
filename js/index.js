60e4170bebb647e34baf36dfe663ab11e337435f
Alexander <zerescas@users.noreply.github.com>
2022-10-18 19:30:22 +0900

    Refactoring

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 const smallSocialsRow = `
     <div class="d-flex">
 
-        <a class="social social--small social--vk  mr-1" href="https://www.vk.com">
+        <a class="social social--small social--vk  mr-2" href="https://www.vk.com">
             <img src="img/social/logo/vk.svg" alt="vk-icon">
         </a>
 
-        <a class="social social--small social--facebook mr-1" href="https://www.twitter.com">
+        <a class="social social--small social--facebook mr-2" href="https://www.twitter.com">
             <img src="img/social/logo/twitter.svg" alt="twitter-icon">
         </a>
 
-        <a class="social social--small social--twitter mr-1" href="https://www.facebook.com">
+        <a class="social social--small social--twitter mr-2" href="https://www.facebook.com">
             <img src="img/social/logo/facebook.svg" alt="facebook-icon">
         </a>
 
-        <a class="social social--small social--instagram mr-1" href="https://www.instagram.com">
+        <a class="social social--small social--instagram mr-2" href="https://www.instagram.com">
             <img src="img/social/logo/instagram.svg" alt="instagram-icon">
         </a>
 
