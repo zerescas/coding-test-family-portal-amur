bd65a5af9ce728c55c182fbff30790b69fdd1d84
Alexander <zerescas@users.noreply.github.com>
2022-10-18 22:16:47 +0900

    Refactor dark mode for social, articles components

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 const smallSocialsRow = `
     <div class="d-flex">
 
-        <a class="social social--small social--dark social--vk mr-2" href="https://www.vk.com" aria-label="vk-link">
+        <a class="social social--small social--vk mr-2" href="https://www.vk.com" aria-label="vk-link">
             <i class="fa-brands fa-vk"></i>
         </a>
 
-        <a class="social social--small social--dark social--facebook mr-2" href="https://www.twitter.com" aria-label="twitter-link">
+        <a class="social social--small social--facebook mr-2" href="https://www.twitter.com" aria-label="twitter-link">
             <i class="fa-brands fa-twitter fa-sm"></i>
         </a>
 
-        <a class="social social--small social--dark social--twitter mr-2" href="https://www.facebook.com" aria-label="facebook-link">
+        <a class="social social--small social--twitter mr-2" href="https://www.facebook.com" aria-label="facebook-link">
             <i class="fa-brands fa-facebook-f fa-sm"></i>
         </a>
 
-        <a class="social social--small social--dark social--instagram mr-2" href="https://www.instagram.com"
+        <a class="social social--small social--instagram mr-2" href="https://www.instagram.com"
             aria-label="instagram-link">
             <i class="fa-brands fa-instagram fa-sm"></i>
         </a>
 
-        <a class="social social--small social--dark social--ok" href="https://www.ok.ru" aria-label="ok-link">
+        <a class="social social--small social--ok" href="https://www.ok.ru" aria-label="ok-link">
             <i class="fa-brands fa-odnoklassniki fa-sm"></i>
         </a>
 
